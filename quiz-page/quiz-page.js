cc8129c39eadb1dcc13dc04ef661ab4b992f85ee
frede <frederickgetueza@gmail.com>
2024-01-19 21:15:23 +0800

    Display summary pane

--- quiz-page/quiz-page.js
+++ quiz-page/quiz-page.js
@@ -1,5 +1,10 @@
+// Summary Pane
+const summaryPane = document.getElementById("summary-pane");
+
+// Buttons
 const nextBtn = document.getElementById("next-btn");
 const backBtn = document.getElementById("back-btn");
+const flagBtn = document.getElementById("flag-btn");
 
 let currentQuestion = 1;
 let score = 0;
@@ -20,6 +25,9 @@ function showNextQuestion() {
     .getElementById(`question${currentQuestion}`)
     .getElementsByTagName("p")[0].innerText;
 
+  answerArr.push(answer);
+  questionArr.push(question);
+
   // checks the answer if it's correct
   checkAnswer();
 
@@ -30,25 +38,26 @@ function showNextQuestion() {
   currentQuestion++;
 
   if (currentQuestion > 3) {
-    showResults();
+    showSummaryPane();
+    // showResults();
   } else {
     document
       .getElementById(`question${currentQuestion}`)
       .classList.add("active");
   }
 
-  answerArr.push(answer);
-
-  if (selectedAnswer) {
-    // console.log("Selected Answer ID:", selectedAnswer.id);
-    console.log("Question: ", question);
-    console.log("Selected Answer Text:", answer);
-    console.log("Arr of answers: ", answerArr);
-  } else {
-    console.log("No answer selected for the current question.");
-  }
+  // if (selectedAnswer) {
+  //   // console.log("Selected Answer ID:", selectedAnswer.id);
+  //   console.log("Question: ", question);
+  //   console.log("Arr of questions:", questionArr);
+  //   console.log("Selected Answer Text:", answer);
+  //   console.log("Arr of answers: ", answerArr);
+  // } else {
+  //   console.log("No answer selected for the current question.");
+  // }
 }
 
+// show previous question
 function showPreviousQuestion() {
   --score;
 
@@ -65,9 +74,11 @@ function showPreviousQuestion() {
 
   // remove the answer from answerArr when back button is pressed
   answerArr.pop();
-  console.log("Arr of answers: ", answerArr);
+  questionArr.pop();
+  console.log("Arr; of answers: ", answerArr);
 }
 
+// check answer
 function checkAnswer() {
   const selectedAnswer = document.querySelector(
     `input[name="q${currentQuestion}"]:checked`
@@ -82,7 +93,34 @@ function checkAnswer() {
   }
 }
 
+// show summary pane
+function showSummaryPane() {
+  document.getElementById("summary-pane").style.display = "block";
+  backBtn.hidden = true;
+  flagBtn.hidden = true;
+
+  summaryPane.innerHTML = `<h2>Summary</h2>`;
+
+  questionArr.forEach((question, index) => {
+    const questionElement = document.createElement("div");
+    questionElement.textContent = question;
+    summaryPane.appendChild(questionElement);
+
+    // checks if there's answer
+    if (answerArr && answerArr[index]) {
+      const answerElement = document.createElement("div");
+      answerElement.textContent = `Answer: ${answerArr[index]}`;
+      summaryPane.appendChild(answerElement);
+    }
+  });
+}
+
+// show results
 function showResults() {
+  // gets total grade of quiz
+  let grade = (score * 100) / 3;
+
   document.getElementById("result-container").style.display = "block";
   document.getElementById("result").innerText = score;
+  document.getElementById("grade").innerText = grade;
 }
